88c09cf05414e68d44e1eee2cb55661ff5fcb222
Mark B <mrbruce9866@gmail.com>
2019-08-03 13:17:57 -0400

    testing pulling from JAWS db

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,25 +3,22 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Player.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Player Comparison",
-        examples: dbExamples
-      });
+    db.OffPlayer.findOne({where:{name: "Tom Brady"}}).then(function(player) {
+      res.render("index", player);
     });
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Player.findOne({ where: { id: req.params.id } }).then(function(dbPlayer) {
-      res.render("example", {
-        example: dbPlayer
-      });
-    });
-  });
+//   // Load example page and pass in an example by id
+//   app.get("/example/:id", function(req, res) {
+//     db.Player.findOne({ where: { id: req.params.id } }).then(function(dbPlayer) {
+//       res.render("example", {
+//         example: dbPlayer
+//       });
+//     });
+//   });
 
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
-  });
+//   // Render 404 page for any unmatched routes
+//   app.get("*", function(req, res) {
+//     res.render("404");
+//   });
 };
