cece2c095fe27faf8e62b523ab0a5d6b9e941d7f
will <wsherrill17@gmail.com>
2019-08-06 19:10:54 -0400

    changes made to backgound added sidebar

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,7 +3,7 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Player.findAll({}).then(function(dbExamples) {
+    db.OffPlayer.findAll({}).then(function(dbExamples) {
       res.render("index", {
         msg: "Player Comparison",
         examples: dbExamples
